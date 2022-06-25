7f9c9946761a4a138e1daeff8491508369c6df0e
CodeWizrd <codewizard001@gmail.com>
2022-06-26 01:15:02 +0530

    Added Test for Footer Links

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,11 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount , mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Home from '@/views/Home.vue'
 
 
 import About from '@/components/Home/About.vue'
 import Card from '@/components/Home/Card.vue'
+import Footer from '@/components/Footer.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -25,5 +26,16 @@ describe('Home.vue', () => {
   it('renders Card component', () => {
     const wrapper = shallowMount(Home)
     expect(wrapper.findComponent(Card)).toBeTruthy()
+  })
+})
+
+describe('Footer.vue',()=>{
+  it('renders 5 Links',()=>{
+    const wrapper = shallowMount(Footer) 
+    expect(wrapper.exists()).toBe(true)
+    const mainDiv = wrapper.get('div')
+    const fDiv = mainDiv.get('div')
+    const links = fDiv.findAll('a')
+    expect(links.length).toBe(5)
   })
 })
